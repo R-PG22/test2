30a297c1f578d2fdddc871b8fc4d3a4bdf69bdb6
Owaki Ryosei <tsukisigure1225@gmail.com>
2025-06-26 15:58:34 +0900

    change

--- test1/src/main.cpp
+++ test1/src/main.cpp
@@ -3,6 +3,6 @@ BufferedSerial pc(USBTX, USBRX, 115200);
 DigitalIn button0(PA_0, PullUp);
  
 int main(){
-    printf("HelloWorld."); // say HelloWorld!!!
+    printf("HelloWorld."); // say HelloWorld!!!!
     return 0;
 }
